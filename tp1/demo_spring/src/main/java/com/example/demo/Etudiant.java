24b4d77c7d9beebea5596322a7b9366ddf1ee110
Ambinintsoa RANDRIANARISOA <randrideal@gmail.com>
2025-11-23 23:39:13 +0100

    Add files via upload

---
+++ tp1/demo_spring/src/main/java/com/example/demo/Etudiant.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+public class Etudiant {
+    private int identifiant;
+    private String nom;
+    private double moyenne;
+
+    public Etudiant(){}
+
+    public Etudiant(int identifiant,  String nom, double moyenne) {
+        this.identifiant = identifiant;
+        this.moyenne = moyenne;
+        this.nom = nom;
+    }
+
+    public int getIdentifiant() {
+        return identifiant;
+    }
+
+    public void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getMoyenne() {
+        return moyenne;
+    }
+
+    public void setMoyenne(double moyenne) {
+        this.moyenne = moyenne;
+    }
+}
---
+++ tp1/demo_spring/src/main/java/com/example/demo/Tp1SaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1SaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1SaeApplication.class, args);
+	}
+
+}
